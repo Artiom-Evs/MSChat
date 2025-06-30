82814086a70f7f968eeefa1c8b74391fb8e4a91a
Artiom Evs <artiom.evs@gmail.com>
2025-06-30 17:04:41 +0200

    Configure default authorization server using ASP.NET Identity and OpenIdDict libraries

---
+++ MSChat.Auth/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MSChat.Auth.Models;
+
+// Add profile data for application users by adding properties to the User class
+public class User : IdentityUser
+{
+}
+
--- MSChat.Auth/Program.cs
+++ MSChat.Auth/Program.cs
@@ -1,6 +1,48 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MSChat.Auth.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthDBConnection") ?? throw new InvalidOperationException("Connection string 'AuthDBConnection' not found.");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options
+            .UseEntityFrameworkCore()
+            .UseDbContext<ApplicationDbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetTokenEndpointUris("connect/token");
+        options.AllowClientCredentialsFlow();
+        options
+            .AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+        options
+            .UseAspNetCore()
+            .EnableTokenEndpointPassthrough();
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDeveloperExceptionPage();
+
+app.UseForwardedHeaders();
+
+app.UseRouting();
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapDefaultControllerRoute();
 
 app.Run();
