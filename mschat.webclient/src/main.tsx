917b555947fcea62557562abaeaed6fdce70834a
Artiom Evs <artiom.evs@gmail.com>
2025-07-01 20:05:17 +0200

    Configure Material UI library in client app

--- mschat.webclient/src/main.tsx
+++ mschat.webclient/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import '@fontsource/roboto/400.css';
+
+const theme = createTheme();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CssBaseline />
+        <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
