cfb207bce3d7789c96b332308d17181603c345cb
Artiom Evs <artiom.evs@gmail.com>
2025-07-02 19:20:02 +0200

    Add default DbContext configuration

---
+++ MSChat.Chat/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MSChat.Chat.Models;
+
+namespace MSChat.Chat.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MSChat.Chat.Models.Chat> Chat { get; set; } = default!;
+    public DbSet<ChatMember> Members { get; set; } = default!;
+    public DbSet<Message> Messages { get; set; } = default!;
+    public DbSet<ChatMemberLink> ChatMemberLinks { get; set; } = default!;
+}
--- MSChat.Chat/Program.cs
+++ MSChat.Chat/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MSChat.Chat.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("ChatDBConnection")
+    ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
