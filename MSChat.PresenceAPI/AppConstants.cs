94a30672c2b06d2d059f71338967e607e949e04b
Artiom Evs <artiom.evs@gmail.com>
2025-07-12 03:33:46 +0200

    Implement default security configuration in the MSChat.PresenceAPI service

---
+++ MSChat.PresenceAPI/AppConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace MSChat.PresenceAPI;
+
+public static class AppConstants
+{
+    public const string Audience = "mschat.presenceapi";
+    public const string DefaultScope = "presenceapi";
+}
--- MSChat.PresenceAPI/Program.cs
+++ MSChat.PresenceAPI/Program.cs
@@ -1,5 +1,65 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using MSChat.PresenceAPI;
+using MSChat.Shared.Auth.Requirements;
+using MSChat.Shared.Configuration.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// get validated app settings
+var authSettings = builder.Configuration.GetAuthSettings();
+var corsSettings = builder.Configuration.GetCorsSettings();
+
+
+builder.Services.
+    AddAuthentication(options =>
+    {
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authSettings.Authority;
+        options.Audience = AppConstants.Audience;
+        options.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
+
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidIssuer = authSettings.ValidIssuer,
+            ValidAudience = AppConstants.Audience,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser()
+        .AddRequirements(new ScopeRequirement(AppConstants.DefaultScope))
+        .Build();
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(corsSettings.AllowedOrigins.Split(","))
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    }); ;
+});
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
