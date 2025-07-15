91c031529a1b1f000b3d60cd898f86861834ea5f
Artiom Evs <artiom.evs@gmail.com>
2025-07-15 07:11:18 +0200

    Implement receiving chat message sent events in the NotificationWorker service

---
+++ MSChat.NotificationWorker/Consumers/ChatMessageSentConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using MSChat.Shared.Events;
+
+namespace MSChat.NotificationWorker.Consumers;
+
+public class ChatMessageSentConsumer : IConsumer<ChatMessageSent>
+{
+    public Task Consume(ConsumeContext<ChatMessageSent> context)
+    {
+        Console.WriteLine(context.Message);
+
+        return Task.CompletedTask;
+    }
+}
--- MSChat.NotificationWorker/Program.cs
+++ MSChat.NotificationWorker/Program.cs
@@ -1,4 +1,26 @@
+using MassTransit;
+using MSChat.Shared.Configuration.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var rmqSettings = builder.Configuration.GetRabbitMQSettings();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumers(typeof(Program).Assembly);
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(rmqSettings.Host, "/", h =>
+        {
+            h.Username(rmqSettings.Username);
+            h.Password(rmqSettings.Password);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
